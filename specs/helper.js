6b20f46d7cac33fa89bf567e107074f9e7c8d41b
Anthony Ng <newyork.anthonyng@gmail.com>
2016-12-01 21:56:29 -0800

    WIP - Get rid of unnecessary files. Add styling for API

--- specs/helper.js
+++
@@ -1,7 +0,0 @@
-React		= require('react/addons');
-TestUtils	= React.addons.TestUtils;
-ReactTabs	= require('../lib/main');
-Tab			= ReactTabs.Tab;
-Tabs		= ReactTabs.Tabs;
-TabList		= ReactTabs.TabList;
-TabPanel	= ReactTabs.TabPanel;
--- specs/main.js
+++
@@ -1,1 +0,0 @@
-require('./tabs.spec.js');
